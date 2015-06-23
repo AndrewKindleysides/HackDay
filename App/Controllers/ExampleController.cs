ccdefdad08ca2f20a28c8d3997ec5e5128cc11c8
Andrew Kindleysides <andrew.kindleysides@advancedcomputersoftware.com>
2015-06-23 09:43:30 +0100

    get members by name

--- App/Controllers/ExampleController.cs
+++ App/Controllers/ExampleController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net;
 using System.Web.Http;
 using App.Models;
@@ -16,13 +17,19 @@ namespace App.Controllers
             return repository.AllMembers();
         }
 
+        [Route("Member/{name}"), HttpGet]
+        public object AllMembers(string name)
+        {
+            return repository.AllMembers().Where(e => e.Name == name);
+        }
+
         [Route("Member"), HttpPost]
         public HttpStatusCode AddNewMember([FromBody]JObject json)
         {
             var members = repository.AllMembers();
             if (members.Any(e => e.Name == json["Name"].Value<string>()))
                 return HttpStatusCode.Found;
-            members.Add(new Member() { Name = json["Name"].Value<string>(), Age = json["Age"].Value<int>() });
+            members.Add(new Member() {Name = json["Name"].Value<string>(), Age = json["Age"].Value<int>() });
             return HttpStatusCode.OK;
         }
     }
